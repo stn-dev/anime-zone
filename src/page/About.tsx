75b4e1916550fa8c80e12fac67a8514d1f348dad
santatra <mariosantatra@gmail.com>
2025-04-01 11:28:31 +0300

    simulate about page contents

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,11 +1,15 @@
 import Container from '../components/Container'
-import SkeletonGroup from '../components/SkeletonGroup'
-
 
 function About() {
   return (
-    <Container tag='section' className='min-h-screen flex items-center justify-center pb-[50px]'>
-      <SkeletonGroup />
+    <Container tag='section' className='min-h-screen flex flex-col justify-center items-center gap-5'>
+      <h1 className='text-4xl font-bold text-neutre text-center ' > What is this site for? </h1>
+      <p className='text-center ' > Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, reiciendis? Esse, in necessitatibus minus iure ea id explicabo doloremque quae amet a pariatur porro facere dolorum placeat, blanditiis nulla labore. Mollitia corporis corrupti ab animi quaerat nisi, doloribus, perferendis voluptate sequi in cupiditate ea, commodi tenetur molestias reiciendis nemo! Enim architecto itaque fugit numquam quasi doloribus eveniet, mollitia ad suscipit? </p>
+      <h2 className='text-3xl font-semibold text-center text-neutre ' > Why did i create this website ? </h2>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quisquam minima magnam, perferendis, ut debitis officiis asperiores ratione aliquam vel iusto ipsam, ad dolores. Maiores saepe velit iure esse dolores atque, molestias officiis delectus expedita eius molestiae? Dolore corrupti ullam deserunt sunt aut atque tempora et perferendis provident similique delectus eaque aliquam obcaecati facere ea rerum architecto adipisci error ex omnis voluptatum, quod voluptatibus? Ullam eligendi labore ratione autem doloribus cum illum dicta aliquam itaque, eius tempora! Doloribus incidunt inventore, possimus sit sapiente, aut magni esse est exercitationem impedit laborum nobis omnis numquam deleniti consectetur? Odit similique ad dolor aliquid. </p>
+      <h2 className='text-3xl font-semibold text-center'> How it work ? </h2>
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque nesciunt magnam id voluptate magni exercitationem odit nulla rem distinctio, reiciendis repudiandae pariatur qui tempore harum consequatur error praesentium numquam mollitia. </p>
+      <h3 className='text-6xl font-bold text-center text-contrasted' > Hope you will enjoy it! </h3>
     </Container>
   )
 }
